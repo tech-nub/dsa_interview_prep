b07c47379ed3632227e67c52424eb928f8febed2
Sahil Gupta <sahilgreen@gmail.com>
2023-11-10 11:29:20 -0500

    LeetCode27.py

---
+++ leetcode/python/easy/arrays/2pointers/LeetCode27.py
@@ -0,0 +1,42 @@
+"""
+Github -> https://github.com/tech-nub/dsa_interview_prep
+Instagram -> https://www.instagram.com/tech_nub/
+"""
+
+""" 
+
+Problem Description -> https://leetcode.com/problems/remove-element
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+ALGO:
+
+1. 2 Pointers approach. One pointer at the index which needs to be filled and 2nd to iterate through the array.
+2. If a needed value is found at 2nd pointer, insert it at the position of 1st pointer and move this pointer forward. 
+3. Else move only the 2nd pointer forward to check the next value
+
+"""
+
+class LeetCode27(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        # This is the position which needs to be filled
+        ptr = 0
+
+        # Loop through the entire array
+        for i in range(len(nums)):
+            # If current number is not we want to remove, then we can
+			# consider it in the final array and add it to the current
+			# position ptr and increment ptr to fill the next position
+            if nums[i] != val:
+                nums[ptr] = nums[i]
+                ptr+=1
+        # Once all the numbers have been iterated through, ptr will
+		# be at 1 position forward to last filled position and
+		# hence ptr will actually signify the length of the good
+		# array instead of index
+        return ptr
