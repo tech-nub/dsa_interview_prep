81888a8ec3d009039eea86be3d30e508c92e7cb2
Sahil Gupta <sahilgreen@gmail.com>
2023-11-16 11:28:18 -0500

    Create LeetCode169.py

---
+++ leetcode/python/easy/arrays/LeetCode169.py
@@ -0,0 +1,53 @@
+"""
+Github -> https://github.com/tech-nub/dsa_interview_prep
+Instagram -> https://www.instagram.com/tech_nub/
+"""
+
+""" 
+
+Problem Description -> https://leetcode.com/problems/majority-element
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+ALGO:
+
+1. This solution makes use of the fact that majority element exists in the array. If it exists, it means its count will be greater than all other numbers combined (more than n/2). Hence if we assume any element to be majority element then if we cancel its count with other elements, it should still be more than 1. 
+2. Iterate over all the elements and assume 1st element to be majority element.
+3. If it matches with other elements, keep increasing the count and if it does not then keep decreasing it.
+4. If count becomes 0 at any time then we have to assume that some other element is majority element and repeat the process.
+5. At the end, the element for which count !=0 is our result.
+
+There is another solution which makes use of Hashmap to count the # of occurance of a character. That solution takes O(n) space.
+
+"""
+
+class LeetCode169(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # Initialize the count and res to 0
+        count = 0
+        res = 0
+
+        # This prpblem states that there will always be a majority element in the array
+        # So there will always be 1 number whose count will be greater than all other
+        # numbers combined. Initially we can assume 1st number to be majority element and 
+        # as we keep iterating through numbers, we increase a count variable if number is same 
+        # as current majority element. If number is not same as current majority element then we
+        # decrease the count. If count becomes 0, then we change the majority element to this element
+        # because previous majority element count was all neutralized by presence of other elements
+        for i in range(len(nums)):
+            # If count == 0, it means we can use the current number to be our result as 
+            # all other numbers are cancelled
+            if(count == 0):
+                res = nums[i]
+            # If current res is same as this number then increase the count of it
+            if(res == nums[i]):
+                count+=1
+            # If not then reduce the count
+            else:
+                count-=1
+        
+        return res
