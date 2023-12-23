48f8efbd15dcbe2f72c63339e887be2b12d8756a
Sahil Gupta <sahilgreen@gmail.com>
2023-12-23 10:45:26 -0500

    LC162

---
+++ leetcode/python/medium/binarysearch/LeetCode162.py
@@ -0,0 +1,43 @@
+"""
+Github -> https://github.com/tech-nub/dsa_interview_prep
+Instagram -> https://www.instagram.com/tech_nub/
+"""
+
+""" 
+
+Problem Description -> https://leetcode.com/problems/find-peak-element
+Time Complexity: O(log(n))
+Space Complexity: O(1)
+
+ALGO:
+
+1. Because we have to get the solution in log(n) runtime, so we have to go with binary search.
+2. Peak element can be corner element as well if its another neigbour is smaller than it.
+3. In binary search, if middle element is greater than its left and right neighbour then that is our result.
+4. Else whichever side is greater than middle element, we will find the result on that side and move the pointers to that side of middle.
+
+"""
+
+class LeetCode162(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l, r = 0, len(nums) - 1
+        # Move to the side, whichever is greater than mid because
+        # peak will only be where we have greater value
+        while l <= r:
+            m = (l+r) // 2
+            # If left neignour is greater, then move right to left
+            if m > 0 and nums[m] < nums[m - 1]:
+                r = m - 1
+            # If right neighbour is greater, then move left to right
+            elif m < len(nums) - 1 and nums[m] < nums[m + 1]:
+                l = m + 1
+            # If mid is > left & right, we founf the result
+            else:
+                return m
+        
+
+
